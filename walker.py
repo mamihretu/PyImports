040e110cefb0860edfe608f0f600000c1d697841
Michael <mamihretu@mun.ca>
2022-08-16 21:18:41 -0400

    cleaned up walking function

--- walker.py
+++ walker.py
@@ -1,42 +1,50 @@
+
+
+
+"""
+TODO: 
+
+_ exclude obvious directories from being explored 
+_ catch exception cases
+"""
+
 import os
 
 
 
 
-
-
-path = "C:\\Users\\abreham\\Desktop\\Django_projects\\asynchronous"
-
-generator = os.walk(path)
-# print(next(generator))
-
-file_set = set()
-
-def directory_walker(generator, file_set):
+def directory_walker(path):
 	""" A function to walk all directories of the 
 	    codebase and put all python files into a list
 	"""
+	try:
+		generator = os.walk(path)
+		file_set = set()
 
-	for root, dirs, files in generator:
-		# print(dirs, ":", files)
-		for file in files:
-			file_set.add(file)
+		def dfs(generator, file_set):
+			"""This nested function will do the actual walking"""
 
-		
-		for directory in dirs:
-			if len(dirs) != 0 and directory != ".git":
-				# print(directory.upper(), ":>",  files)
-				path = os.path.join(root, directory)
-				print(path)
-				new_generator = os.walk(path)
-				directory_walker(new_generator, file_set)
+			for root, dirs, files in generator:
+				for file in files:
+					file_set.add(file)
 
-
-
-	return file_set
+				
+				for directory in dirs:
+					if len(dirs) != 0 and directory != ".git":
+						path = os.path.join(root, directory)
+						new_generator = os.walk(path)
+						dfs(new_generator, file_set)
 
 
-print(directory_walker(generator, file_set))
+			return file_set
+
+		return dfs(generator, file_set)
+
+	except:
+		pass
+
+
+
 
 
 
