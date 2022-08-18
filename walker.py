a0de8f11ab6b6e5feff791983ffe78d238047268
Michael <mamihretu@mun.ca>
2022-08-18 15:03:56 -0400

    optimized directory walker so that no directory is walked more than once

--- walker.py
+++ walker.py
@@ -1,41 +1,56 @@
 import os
 
 
+
+
+
 def directory_walker(path):
 	""" A function to walk all directories of the 
 	    codebase and put all python files into a list
-	    parameters: path -> str
+	    parameters: path : str
 	    result: Set(str)
 	"""
 	try:
 		generator = os.walk(path)
-		file_path_set = set()
+		file_dict = {}
+		directory_path_set = set()
+		directory_path_set.add(path)
 
-		def dfs(generator, file_path_set):
-			"""This nested function will do the actual walking"""
+		def dfs(generator, file_dict):
+			"""This function will do the actual walking
+			   parameters -> generator: generator object( generator[0] = root, generator[1] = list(dirs), generator[2] = list(files)) 
+			              -> file_dict: empty dictionary object
+			   result -> dictionary of path to file mapping 
+			"""
 
 			for root, dirs, files in generator:
-				for file in files:
+
+				for file in files:               # Add path to file mapping to file_dict
 					if is_py_file(file):
 						path = os.path.join(root, file)
-						file_path_set.add(path)
+						file_dict[path] = file 
 
 
-				
-				for directory in dirs:
+				for directory in dirs:           # List files and directories of one directory
 					if len(dirs) != 0 and directory != ".git":
 						path = os.path.join(root, directory)
-						new_generator = os.walk(path)
-						dfs(new_generator, file_path_set)
+						if path not in directory_path_set:
+							directory_path_set.add(path)
+							new_generator = os.walk(path)
+							dfs(new_generator, file_dict)
+						else:
+							pass
 
-			return file_path_set
+			return file_dict	
 
-		python_set = dfs(generator, file_path_set)
+		python_dict = dfs(generator, file_dict)
 
-		return python_set
+		return python_dict
 
 	except:
 		pass
+
+
 
 
 
